584aa7d2661e71ca0f157fd371746816aed3223f
RaamPrashathT <Raamprashath.Thirunavukkarasu@kovanlabs.in>
2026-07-03 13:06:11 +0530

    build error fix

--- src/modules/attendance/components/AttendancePageShell.tsx
+++ src/modules/attendance/components/AttendancePageShell.tsx
@@ -335,8 +335,6 @@ export function AttendancePageShell({
               </AlertDialogFooter>
             </AlertDialogContent>
           </AlertDialog>
-        </div>
-      </main>
     </>
   );
 }
